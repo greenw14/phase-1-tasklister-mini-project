499bc2eed0680c6c8d2e60770efb1955b3bf6782
william <willhgreen02@gmail.com>
2021-10-13 10:00:12 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#create-task-form").addEventListener('submit', (e) => {
+    e.preventDefault()
+    let li = document.createElement('li')
+    li.textContent = e.target["new-task-description"].value
+    document.querySelector('#tasks').append(li)
+  })
 });
